38428c758a2dd6c91d8fb946fe3e344c689819a6
nhlekzin22 <nhlakaniphoblessing16@gmail.com>
2023-05-11 14:19:26 +0200

    adding create blog code

---
+++ myfunctions/create_blog.py
@@ -0,0 +1,49 @@
+import boto3
+import json
+from custom_encoder import CustomEncoder
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodbTableName = 'websiteUserDetails'
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(dynamodbTableName)
+
+postMethod = 'POST'
+blogPath = '/blog'
+
+def lambda_handler(event, context):
+    logger.info(event)
+    httpMethod = event['httpMethod']
+    path = event['path']
+    if httpMethod == postMethod and path == blogPath:
+        response = insertBlog(json.loads(event['body']))
+    else:
+        response = buildResponse(404, 'NotFound')
+    
+    return response
+
+def insertBlog(requestBody):
+    try:
+        table.put_item(Item=requestBody)
+        body = {
+            'Operation': 'SAVE',
+            'Message': 'SUCCESS',
+            'Item' : requestBody
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('Do custom error handlin here')
+
+
+def buildResponse(statusCode, body=None):
+    response = {
+        'statusCode': statusCode,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+            }
+        }
+    if body is not None:
+        response['body'] = json.dumps(body, cls=CustomEncoder)
+    return response
