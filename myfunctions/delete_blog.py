747aa98afd0d075b26454add296cc2918223b433
mayurmis3 <mayurmis3@gmail.com>
2023-05-11 13:48:30 +0200

    adding delete blog

---
+++ myfunctions/delete_blog.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+
+def delete_handler(event, context):
+    # TODO implement
+    client_dynamo=boto3.resource('dynamodb')
+    table=client_dynamo.Table('BlogTable')
+    id=event['id'] # this will delete row with partition key
+    try:
+        response=table.delete_item(Key={"id":id})
+        return "Done"
+    except:
+        raise
