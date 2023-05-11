b1e459b42741f637df9ba7afd9bd15102670cfa1
Given Makofane <makofane411@gmail.com>
2023-05-11 14:06:35 +0200

    Revert "test branch"
    This reverts commit 240a6bfa8cab22a1cf600d00c96d9df27c5bfe4f.

--- myfunctions/app.py
+++ myfunctions/app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
       return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "working now bracn workflowgi",
+            "message": "working now bracn workflowgit",
             # "location": ip.text.replace("\n", "")
         }),
     }
