a818627b4a5471328e30df21b196bf420d72e772
Given Makofane <makofane411@gmail.com>
2023-05-11 11:06:38 +0200

    adding dyanmo db

--- myfunctions/app.py
+++ myfunctions/app.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
       return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello actions worked checking dynamo now",
+            "message": "hello actions worked ./ checking dynamo now",
             # "location": ip.text.replace("\n", "")
         }),
     }
