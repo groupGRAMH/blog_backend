cbf9cb911ccf72e638152b6b1e684041ff6c93a1
mayurmis3 <mayurmis3@gmail.com>
2023-05-12 10:09:21 +0200

    message

--- myfunctions/delete_blog.py
+++ myfunctions/delete_blog.py
@@ -4,7 +4,7 @@ import boto3
 def delete_handler(event, context):
     # TODO implement
     client_dynamo=boto3.resource('dynamodb')
-    table=client_dynamo.Table('BlogTable')
+    table=client_dynamo.Table('BlogTable ')
     id=event['id'] # this will delete row with partition key
     try:
         response=table.delete_item(Key={"id":id})
