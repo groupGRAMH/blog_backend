695f38fc375ad8aea7c0c9021f00efcccbdc9c09
Given Makofane <makofane411@gmail.com>
2023-05-22 09:28:09 +0200

    fixing  gget

--- myfunctions/get_blogs.py
+++ myfunctions/get_blogs.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+from decimal import Decimal
 
 dynamodb_table_name = 'BlogTable'
 dynamodb = boto3.resource('dynamodb')
@@ -9,14 +10,17 @@ def get_handler(event, context):
     if event['httpMethod'] != 'GET':
         raise Exception(f"getMethod only accepts GET method, you tried: {event['httpMethod']} method.")
 
-    
     response = table.scan()
-
     items = response.get('Items', [])
 
-    
+    # Convert Decimal objects to string representation
+    serialized_items = []
+    for item in items:
+        serialized_item = {k: str(v) if isinstance(v, Decimal) else v for k, v in item.items()}
+        serialized_items.append(serialized_item)
+
     body = {
-        'blogs': items
+        'blogs': serialized_items
     }
 
     response = {
